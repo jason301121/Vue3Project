791aa014110ad0cdc017b2bd19964397e1518181
Jason <jason301121@gmail.com>
2021-09-06 14:21:43 +0800

    建立後台

---
+++ src/views/Back/Dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+dashboard
+</template>
+
+<script>
+export default {
+    created() {
+        const token = document.cookie.replace(/(?:(?:^|.*;\s*)buyFresh\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+        this.$http.defaults.headers.common['Authorization'] = token;
+        const api=`${process.env.VUE_APP_API}api/user/check`
+            this.$http.post(api,this.user)
+            .then((res)=>{
+                console.log(res);
+                if(!res.data.success){
+                    this.$router.push('/login');
+                }
+            });
+    },
+}
+</script>
---
+++ src/views/Back/ProductManage.vue
@@ -0,0 +1,3 @@
+<template>
+ProductManage
+</template>
